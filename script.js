5db12724c701b350a4588f93554ef141d06c14d2
augusto-mattos <guto.s.mattos@gmail.com>
2022-08-01 17:29:29 +0200

    feat(html-css-js)finished pages

---
+++ script.js
@@ -0,0 +1,21 @@
+$(document).ready(() => {
+    const slick0ptions = {
+        autoplay: true,
+        dots: false,
+        prevArrow: '<button type="button" class="slick-prev slider__prev-arrow">Previous</button>;',
+        nextArrow: '<button type="button" class="slick-next slider__next-arrow">Previous</button>;'
+    };
+    $(".slider").slick(slick0ptions)
+    
+  });
+
+  $(document).ready(() => {
+    const slick0ptions = {
+        autoplay: true,
+        dots: true,
+        prevArrow: '<button type="button" class="slick-prev slider__prev-arrow">Previous</button>;',
+        nextArrow: '<button type="button" class="slick-next slider__next-arrow">Previous</button>;'
+    };
+    $(".slider__testemonials").slick(slick0ptions)
+    
+  });
