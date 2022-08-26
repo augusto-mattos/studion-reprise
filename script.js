04f4f9b0877463727ef2adadb1ff6e8e991ad45b
augusto-mattos <guto.s.mattos@gmail.com>
2022-08-26 16:08:39 +0200

    feat(navbar mobile)

--- script.js
+++ script.js
@@ -1,4 +1,34 @@
-$(document).ready(() => {
+/* $(document).ready(() => {
+    const slick0ptions = {
+        autoplay: true,
+        dots: false,
+        prevArrow: '<button type="button" class="slick-prev slider__prev-arrow">Previous</button>;',
+        nextArrow: '<button type="button" class="slick-next slider__next-arrow">Previous</button>;'
+    };
+    $(".slider").slick(slick0ptions)
+    
+  });
+
+  $(document).ready(() => {
+    const slick0ptions = {
+        autoplay: true,
+        dots: true,
+        prevArrow: '<button type="button" class="slick-prev slider__prev-arrow">Previous</button>;',
+        nextArrow: '<button type="button" class="slick-next slider__next-arrow">Previous</button>;'
+    };
+    $(".slider__testemonials").slick(slick0ptions)
+    
+  });
+  */
+
+  $(document).ready(() => {
+
+    $(".navbar__menu-btn").on("click", function () {
+      $(".navbar__links").toggleClass("active");
+      $(this).find('i').toggleClass('fa-bars');
+      $(this).find('i').toggleClass('fa-times');
+    })
+
     const slick0ptions = {
         autoplay: true,
         dots: false,
